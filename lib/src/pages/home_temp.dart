53675727ca42d61b0a8fd59fbad69cc0fa7a3822
Hernandez Carmona Samuel Karim <mb78200@MB78200MAC.local>
2019-11-24 14:52:33 -0600

    List View from static array on Home page temporal

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+
 class HomePageTemp extends StatelessWidget {
-  const HomePageTemp({Key key}) : super(key: key);
+
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
 
   @override
   Widget build(BuildContext context) {
@@ -10,20 +13,37 @@ class HomePageTemp extends StatelessWidget {
         title: Text('Componentes Temp'),
         ),
         body: ListView(
-          children: <Widget>[
-            ListTile(
-              title: Text('ListTile Title'),
-            ),
-            Divider(),
-            ListTile(
-              title: Text('ListTile Title'),
-            ),
-            Divider(),
-            ListTile(
-              title: Text('ListTile Title'),
-            ),
-          ],
+          children: _crearItems()
         ),
     );
   }
+
+
+  List<Widget> _crearItems() {
+
+
+    List<Widget> lista = new List<Widget>();
+
+    for (String opt in opciones) {
+
+      final tempWidget = ListTile(
+        title: Text( opt ),
+      );
+
+      lista..add( tempWidget )
+           ..add( Divider() );
+
+      
+    }
+
+    return lista;
+  }
+
+  List<Widget> crearItemsCorta() {
+
+
+    
+  }
+
+
 }
